14de5aa8ebc4d157c088cbf3600635825468374b
OdinM13 <markodinius@gmail.com>
2025-09-21 21:39:17 +0200

    Started building playround function. Added case unsensitivity

--- main.js
+++ main.js
@@ -3,14 +3,34 @@ let computerScore = 0;
 const MAX = 3; // 3 because of 3 choices: rock, paper, scissors
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * MAX);
+    computerChoice = Math.floor(Math.random() * MAX);
+    if (computerChoice == 0) {
+        return "rock";
+    }
+    if (computerChoice == 1) {
+        return "paper";
+    }
+    if (computerChoice == 2) {
+        return "scissors";
+    }
+        
 }
 
 function getHumanChoice() {
-    return prompt("Choose between Rock, Paper or Scissors");
+    humanChoice = prompt("Choose between Rock, Paper or Scissors").toLowerCase();
+    return humanChoice;
 }
 
 function playRound (humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Scissors");
+    }
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock");
+    }
+    if (humanChoice == computerChoice) {
+        console.log("Tie! You and the computer chose", humanChoice);
+    }
     return;
 }
 
